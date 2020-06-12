9dc8568bafaaba6548060f1e069cfbd688445364
magalimenchon <magalimenchon@gmail.com>
2020-06-12 20:29:57 -0300

    add

---
+++ Ejercicio 3/servicio.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", StartPage);
+
+function StartPage(){
+    "use strict";
+    let reservas = {
+        "pedidos" : [
+                    {
+                        "patente": "WEB765",
+                        "cochera": 8,
+                        "carpa": 24
+                    },
+                    {
+                        "patente": "URL231",
+                        "cochera": 2,
+                        "carpa": 30
+                    },
+        ]
+    }
+    /*....................funciones...........................*/
+    function ObtenerCochera(carpa){
+        for(let i = 0; i < reservas.pedidos.length; i++){
+            if(reservas.pedidos[i].carpa === carpa){
+                console.log("La cochera de la carpa " + carpa + " es la " + reservas.pedidos[i].cochera);
+            }
+        }
+    }
+    function AgregarPedido(){
+        let pedido = CrearPedido(reservas);
+        reservas.pedidos.push(pedido);
+    }
+    function CrearPedido(){
+        let item = {
+            "patente": "BDY451",
+            "cochera": 9,
+            "carpa": 35
+        }
+        return item;
+    }
+    /*....................START.....................*/ 
+    ObtenerCochera(24);
+    AgregarPedido();
+}
